5b38a06ae1e0baa9ef2a89b6b137b46891ae568d
charlieTUX <charlie.houdmond@outlook.fr>
2026-03-19 16:56:05 +0100

    format

--- flake.nix
+++ flake.nix
@@ -4,58 +4,61 @@
   inputs = {
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    { self, nixpkgs }:
+    {
 
-    overlays = f: p: {
-       signal-desktop = let 
-         style = ./themes.css;
-	 name = "signal-desktop-dracula";
-	 version = p.signal-desktop.version;
-	 src = p.signal-desktop.src;
-	 asarSource = "share/signal-desktop/app.asar";
-	 styleSource = "stylesheets/manifest.css";
-       in p.stdenv.mkDerivation (f: {
-         inherit name version src;
-	 nativeBuildInputs = with p; [
-	    makeWrapper
-	    asar
-         ];
-	 installPhase = ''
-	   cp -r ${p.signal-desktop}/share $out
-	   asar e $out/${asarSource} $out/share/temp
-	   rm ${asarSource}
-	   sed -i "1i @import \"${style}\";" "$out/share/temp/${styleSource}"
-	   asar p $out/share/temp ${asarSource}
-	   makeWrapper '${p.lib.getExe p.electron_39}' "$out/bin/signal-desktop" \
-	     --add-flags "$out/app_custom.asar" \
-             --set-default ELECTRON_FORCE_IS_PACKAGED 1 \
-             --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}" \
-	'';
-	desktopItems = [
-    (f.makeDesktopItem {
-      name = "signal";
-      desktopName = "Signal";
-      exec = "signal-desktop %U";
-      type = "Application";
-      terminal = false;
-      icon = "signal-desktop";
-      comment = "Private messaging from your desktop";
-      startupWMClass = "signal";
-      mimeTypes = [
-        "x-scheme-handler/sgnl"
-        "x-scheme-handler/signalcaptcha"
-      ];
-      categories = [
-        "Network"
-        "InstantMessaging"
-        "Chat"
-      ];
-    })
-  ];
+      overlays = f: p: {
+        signal-desktop =
+          let
+            style = ./themes.css;
+            name = "signal-desktop-dracula";
+            version = p.signal-desktop.version;
+            src = p.signal-desktop.src;
+            asarSource = "share/signal-desktop/app.asar";
+            styleSource = "stylesheets/manifest.css";
+          in
+          p.stdenv.mkDerivation (f: {
+            inherit name version src;
+            nativeBuildInputs = with p; [
+              makeWrapper
+              asar
+            ];
+            installPhase = ''
+              	   cp -r ${p.signal-desktop}/share $out
+              	   asar e $out/${asarSource} $out/share/temp
+              	   rm ${asarSource}
+              	   sed -i "1i @import \"${style}\";" "$out/share/temp/${styleSource}"
+              	   asar p $out/share/temp ${asarSource}
+              	   makeWrapper '${p.lib.getExe p.electron_39}' "$out/bin/signal-desktop" \
+              	     --add-flags "$out/app_custom.asar" \
+                           --set-default ELECTRON_FORCE_IS_PACKAGED 1 \
+                           --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}" \
+              	'';
+            desktopItems = [
+              (f.makeDesktopItem {
+                name = "signal";
+                desktopName = "Signal";
+                exec = "signal-desktop %U";
+                type = "Application";
+                terminal = false;
+                icon = "signal-desktop";
+                comment = "Private messaging from your desktop";
+                startupWMClass = "signal";
+                mimeTypes = [
+                  "x-scheme-handler/sgnl"
+                  "x-scheme-handler/signalcaptcha"
+                ];
+                categories = [
+                  "Network"
+                  "InstantMessaging"
+                  "Chat"
+                ];
+              })
+            ];
 
+          });
+      };
 
-       };)
     };
-    
-  };
 }
